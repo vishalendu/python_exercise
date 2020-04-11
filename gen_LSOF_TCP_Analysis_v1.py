2a3c41ba2a0512cb1b0c711d232354b39bb75fc4
Vishalendu Pandey <vishalendupandey@Vishalendus-MacBook-Pro.local>
2020-04-10 20:11:09 -0500

    adding memory optimized lsof analysis script

--- gen_LSOF_TCP_Analysis_v1.py
+++ gen_LSOF_TCP_Analysis_v1.py
@@ -8,20 +8,24 @@ def generateTCPSummary(infile,outfile):
     # Read lsof
     cols=['COMMAND' ,'PID','TID', 'USER', 'FD' ,'TYPE' ,'DEVICE' \
                    ,'SIZE/OFF' ,'NODE', 'NETWORK_INFO' ,'CONN_STATUS']
+    dt = {'PID':'object','TID':'category','USER':'category','FD':'category',\
+          'TYPE':'category','DEVICE':'category','SIZE/OFF':'category','CONN_STATUS':'category',\
+          'NODE':'category','COMMAND':'object','NETWORK_INFO':'object'}
     lsof1 = pd.read_csv(infile,sep="\s+",\
-                        header=None,skiprows=1,names=cols)
+                        header=None,skiprows=1,names=cols,dtype=dt)
     print(f'After loading file Memory = {getCurrentMemoryUsage()/(1024*1024)} MB')
 
     # Filter by TCP and extract TO/FROM IP/PORTS
     tcp = lsof1[(lsof1['CONN_STATUS']!='(LISTEN)') & (lsof1['NODE']=='TCP')]
     tcp = pd.concat([tcp,tcp['NETWORK_INFO'].str.extract(r'(.+?):(.+?)->(.+?):(.+?)$')],axis=1).\
     rename(columns={0:'FROM_IP',1:'FROM_PORT',2:'TO_IP',3:'TO_PORT'})
+    for i in ['FROM_IP','FROM_PORT','TO_IP','TO_PORT']:
+        tcp[i]=tcp[i].astype('category')
+    tcp['COUNT'] = 1
     print(f'After formatting dataframe Memory = {getCurrentMemoryUsage()/(1024*1024)} MB')
 
     # Generate summary
-    ip_summary = pd.DataFrame(tcp.groupby(['PID','COMMAND','FROM_IP','TO_IP','CONN_STATUS','TYPE']).\
-       COMMAND.count()).rename(columns={'COMMAND':'COUNT'}).sort_values('COUNT',ascending=False)
-    ip_summary.to_csv(outfile)
+    pd.crosstab([tcp.PID,tcp.COMMAND,tcp.FROM_IP,tcp.TO_IP,tcp.CONN_STATUS,tcp.TYPE],tcp.COUNT).sort_values(1,ascending=False).to_csv(outfile)
     print(f'Final Memory = {getCurrentMemoryUsage()/(1024*1024)} MB')
 
 infile = '/Users/vishalendupandey/Downloads/lsof/lsof.txt'
